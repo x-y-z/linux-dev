7a8d160109402dfda1ac4ea35b88630540580a0c
Alistair Popple <apopple@nvidia.com>
2025-01-07 14:42:34 +1100

    mm/gup: don't allow FOLL_LONGTERM pinning of FS DAX pages
    Longterm pinning of FS DAX pages should already be disallowed by various
    pXX_devmap checks.  However a future change will cause these checks to be
    invalid for FS DAX pages so make folio_is_longterm_pinnable() return false
    for FS DAX pages.
    
    Link: https://lkml.kernel.org/r/8bd0a8c0f62e2d2d8ece6f445580d20b73e3decd.1736221254.git-series.apopple@nvidia.com
    Signed-off-by: Alistair Popple <apopple@nvidia.com>
    Reviewed-by: John Hubbard <jhubbard@nvidia.com>
    Acked-by: David Hildenbrand <david@redhat.com>
    Cc: Asahi Lina <lina@asahilina.net>
    Cc: Bjorn Helgaas <bhelgaas@google.com>
    Cc: Björn Töpel <bjorn@rivosinc.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Christoph Hellwig <hch@lst.de>
    Cc: Chunyan Zhang <zhang.lyra@gmail.com>
    Cc: Dan Wiliams <dan.j.williams@intel.com>
    Cc: Darrick J. Wong <djwong@kernel.org>
    Cc: Dave Chinner <david@fromorbit.com>
    Cc: Dave Hansen <dave.hansen@linux.intel.com>
    Cc: Dave Jiang <dave.jiang@intel.com>
    Cc: Gerald Schaefer <gerald.schaefer@linux.ibm.com>
    Cc: Ira Weiny <ira.weiny@intel.com>
    Cc: Jan Kara <jack@suse.cz>
    Cc: Jason Gunthorpe <jgg@nvidia.com>
    Cc: Jason Gunthorpe <jgg@ziepe.ca>
    Cc: kernel test robot <lkp@intel.com>
    Cc: Logan Gunthorpe <logang@deltatee.com>
    Cc: Matthew Wilcox (Oracle) <willy@infradead.org>
    Cc: Miaohe Lin <linmiaohe@huawei.com>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Peter Xu <peterx@redhat.com>
    Cc: Theodore Ts'o <tytso@mit.edu>
    Cc: Vishal Verma <vishal.l.verma@intel.com>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- include/linux/mm.h
+++ include/linux/mm.h
@@ -2099,6 +2099,10 @@ static inline bool folio_is_longterm_pinnable(struct folio *folio)
 	if (folio_is_device_coherent(folio))
 		return false;
 
+	/* DAX must also always allow eviction. */
+	if (folio_is_fsdax(folio))
+		return false;
+
 	/* Otherwise, non-movable zone folios can be pinned. */
 	return !folio_is_zone_movable(folio);
 
