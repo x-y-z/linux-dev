950baa49e596016dfaf7c48f2df46c0dc42ec047
Zi Yan <ziy@nvidia.com>
2025-09-23 17:47:43 +0000

    adjust NR_MAX_BATCHED_MIGRATION for testing
    change NR_MAX_BATCHED_MIGRATION to HPAGE_PUD_NR to allow batching THP
    copies.
    
    These are for testing purpose only.
    
    Signed-off-by: Shivank Garg <shivankg@amd.com>
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- mm/migrate.c
+++ mm/migrate.c
@@ -1670,7 +1670,7 @@ static inline int try_split_folio(struct folio *folio, struct list_head *split_f
 }
 
 #ifdef CONFIG_TRANSPARENT_HUGEPAGE
-#define NR_MAX_BATCHED_MIGRATION	HPAGE_PMD_NR
+#define NR_MAX_BATCHED_MIGRATION	HPAGE_PUD_NR
 #else
 #define NR_MAX_BATCHED_MIGRATION	512
 #endif
