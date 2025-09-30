d8795075cebbfc6a0769857d727325149a2f38b9
Zi Yan <ziy@nvidia.com>
2025-09-29 22:55:06 -0400

    mm/huge_memory: do not change split_huge_page*() target order silently.
    Page cache folios from a file system that support large block size (LBS)
    can have minimal folio order greater than 0, thus a high order folio might
    not be able to be split down to order-0. Commit e220917fa507 ("mm: split a
    folio in minimum folio order chunks") bumps the target order of
    split_huge_page*() to the minimum allowed order when splitting a LBS folio.
    This causes confusion for some split_huge_page*() callers like memory
    failure handling code, since they expect after-split folios all have
    order-0 when split succeeds but in really get min_order_for_split() order
    folios.
    
    Fix it by failing a split if the folio cannot be split to the target order.
    
    Fixes: e220917fa507 ("mm: split a folio in minimum folio order chunks")
    Reported-by: syzbot+e6367ea2fdab6ed46056@syzkaller.appspotmail.com
    Closes: https://lore.kernel.org/all/68d2c943.a70a0220.1b52b.02b3.GAE@google.com/
    Signed-off-by: Zi Yan <ziy@nvidia.com>

--- include/linux/huge_mm.h
+++ include/linux/huge_mm.h
@@ -365,34 +365,16 @@ int folio_split(struct folio *folio, unsigned int new_order, struct page *page,
  * Return: 0: split is successful, otherwise split failed.
  */
 static inline int try_folio_split(struct folio *folio, struct page *page,
-		struct list_head *list)
+		struct list_head *list, unsigned int order)
 {
-	int ret = min_order_for_split(folio);
-
-	if (ret < 0)
-		return ret;
-
-	if (!non_uniform_split_supported(folio, 0, false))
+	if (!non_uniform_split_supported(folio, order, false))
 		return split_huge_page_to_list_to_order(&folio->page, list,
-				ret);
-	return folio_split(folio, ret, page, list);
+				order);
+	return folio_split(folio, order, page, list);
 }
 static inline int split_huge_page(struct page *page)
 {
-	struct folio *folio = page_folio(page);
-	int ret = min_order_for_split(folio);
-
-	if (ret < 0)
-		return ret;
-
-	/*
-	 * split_huge_page() locks the page before splitting and
-	 * expects the same page that has been split to be locked when
-	 * returned. split_folio(page_folio(page)) cannot be used here
-	 * because it converts the page to folio and passes the head
-	 * page to be split.
-	 */
-	return split_huge_page_to_list_to_order(page, NULL, ret);
+	return split_huge_page_to_list_to_order(page, NULL, 0);
 }
 void deferred_split_folio(struct folio *folio, bool partially_mapped);
 
