72dc76018d0e07edf4dee14aa2ec692a917a51bf
Zi Yan <ziy@nvidia.com>
2025-11-26 15:24:26 -0500

    Revert "mm/huge_memory: make min_order_for_split() always return an order"
    This reverts commit b59752855c7b126c59a68c06e7a431a67f6ee134.

--- mm/huge_memory.c
+++ mm/huge_memory.c
@@ -4231,29 +4231,16 @@ int folio_split(struct folio *folio, unsigned int new_order,
 			     SPLIT_TYPE_NON_UNIFORM);
 }
 
-/**
- * min_order_for_split() - get the minimum order @folio can be split to
- * @folio: folio to split
- *
- * min_order_for_split() tells the minimum order @folio can be split to.
- * If a file-backed folio is truncated, 0 will be returned. Any subsequent
- * split attempt should get -EBUSY from split checking code.
- *
- * Return: @folio's minimum order for split
- */
-unsigned int min_order_for_split(struct folio *folio)
+int min_order_for_split(struct folio *folio)
 {
 	if (folio_test_anon(folio))
 		return 0;
 
-	/*
-	 * If the folio got truncated, we don't know the previous mapping and
-	 * consequently the old min order. But it doesn't matter, as any split
-	 * attempt will immediately fail with -EBUSY as the folio cannot get
-	 * split until freed.
-	 */
-	if (!folio->mapping)
-		return 0;
+	if (!folio->mapping) {
+		if (folio_test_pmd_mappable(folio))
+			count_vm_event(THP_SPLIT_PAGE_FAILED);
+		return -EBUSY;
+	}
 
 	return mapping_min_folio_order(folio->mapping);
 }
