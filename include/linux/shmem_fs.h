2d3bd39f56abab5c4bd54510e6729573b310b62e
Zi Yan <ziy@nvidia.com>
2026-05-07 03:35:33 -0400

    Revert "mm/khugepaged: remove READ_ONLY_THP_FOR_FS check in hugepage_enabled()"
    This reverts commit 83f151d1f7e3429100d148fbafc65203294e9f90.

--- include/linux/shmem_fs.h
+++ include/linux/shmem_fs.h
@@ -127,7 +127,7 @@ int shmem_writeout(struct folio *folio, struct swap_iocb **plug,
 void shmem_truncate_range(struct inode *inode, loff_t start, uoff_t end);
 int shmem_unuse(unsigned int type);
 
-#if defined(CONFIG_TRANSPARENT_HUGEPAGE) && defined(CONFIG_SHMEM)
+#ifdef CONFIG_TRANSPARENT_HUGEPAGE
 unsigned long shmem_allowable_huge_orders(struct inode *inode,
 				struct vm_area_struct *vma, pgoff_t index,
 				loff_t write_end, bool shmem_huge_force);
